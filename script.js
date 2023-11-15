b8db60ea470c25d437c20fafc8a06a8b2dc45851
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-15 17:34:08 +0100

    feat: cambio colore on click

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // FUNZIONI
 
 // Funzione che crea degli elementi con possoibilitá di cambiare tag contenuto o classe
-function createElement(tag, content, className) {
+function createElement(tag, content, className, classNameAdd) {
     const element = document.createElement(tag);
     element.classList.add(className);
     element.append(content);
@@ -9,6 +9,9 @@ function createElement(tag, content, className) {
       //Event listener che aggiunge un evento click a ogni div creato
       element.addEventListener('click', function() {
         console.log(this.textContent);
+
+        // Aggiunge una classe agli elementi cliccati
+        this.classList.add(classNameAdd);
     });
 
     return element;
@@ -21,6 +24,8 @@ const board = document.querySelector('.gameboard');
 
 // Uso il ciclo for per creare 100 celle all'interno del div
 for (i = 1; i <= 100; i++){
-    const myElement = createElement('div', i, 'cell');
+    const myElement = createElement('div', i, 'cell', 'clickcolor');
     board.append(myElement)
+
+
 }
